6b5162d7f1ae66408a056c27493626635cc13948
Yevhen Stativka <stativkaea@gmail.com>
2017-04-19 18:27:43 +0300

    Use mongoose validations instead of custom ones

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
 
-const TopicSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		unique: true
+const TopicSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true
+		}
+	},
+	{
+		strict: 'throw' // throw error, if field is not specified in the schema
 	}
-})
+)
 
 export default mongoose.model('Topic', TopicSchema)
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -5,41 +5,21 @@ const topics = Router() // create a '/topics' route
 
 
 // responds with all topics from the database
-topics.get('/', (req, res) => {
-	db.getTopics()
-		.then(data => res.json(data))
-		.catch(() => res.status(500).send('db error'))
-})
+topics.get('/', (req, res) => db.getTopics()
+	.then(data => res.json(data))
+	.catch(() => res.status(500).send('db error')))
 
 
 // inserts new topic to the database and responds with newly created topic
-topics.post('/', (req, res) => {
-	const name = req.body.name
-
-	if (typeof name !== 'string') {
-		return res.status(400).send('Invalid request')
-	}
-
-	const newTopic = { name }
-
-	db.insertTopic(newTopic)
-		.then(topic => res.send(topic))
-		.catch(err => res.status(500).send(err))
-})
+topics.post('/', (req, res) => db.insertTopic(req.body.newTopicProps)
+	.then(topic => res.send(topic))
+	.catch(err => res.status(500).send(err)))
 
 
 // updates a topic specified by id with provided properties
-topics.put('/:id', (req, res) => {
-	const updatedProps = req.body.updatedProps
-
-	if (!(updatedProps instanceof Object) || updatedProps instanceof Array) {
-		res.status(500).send('Incorrect request (\'updatedTopic\' is not an object')
-	}
-
-	db.updateTopicById(req.params.id, updatedProps)
-		.then(() => res.sendStatus(200))
-		.catch(error => res.status(500).send({ errorMessage: 'Database error', error }))
-})
+topics.put('/:id', (req, res) => db.updateTopicById(req.params.id, req.body.updatedProps)
+	.then(topic => res.send(topic))
+	.catch(err => res.status(500).send(err)))
 
 
 export default topics
