77d1aab4c2231fe7730cc55011cddd2d987f56ab
Stativka, Yavhen <stativkaea@gmail.com>
2017-05-19 12:03:24 +0300

    Add separate endpoint for votes (likes in future)

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -16,10 +16,32 @@ topics.post('/', (req, res) => db.insertTopic(req.body.newTopicProps)
 	.catch(err => res.status(500).send(err)))
 
 
-// updates a topic specified by id with provided properties
-topics.put('/:id', (req, res) => db.updateTopicById(req.params.id, req.body.updatedProps)
-	.then(topic => res.send(topic))
-	.catch(err => res.status(500).send(err)))
+// updates a topic specified by id with provided properties, except points
+topics.put('/:id', (req, res) => {
+	if (!(req.body.updatedTopicProps instanceof Object) || req.body.updatedTopicProps instanceof Array) {
+		return res.status(400).send('\'updatedTopicProps\' should be an object')
+	}
+	
+	if (req.body.updatedTopicProps.points) {
+		return res.status(400).send('use \'/:id/points\' endpoint for update of points')
+	}
+
+	db.updateTopicById(req.params.id, req.body.updatedTopicProps)
+		.then(topic => res.send(topic))
+		.catch(err => res.status(500).send(err))
+})
+
+
+// updates points of a topic specified by id
+topics.put('/:id/points', (req, res) => {
+	if (req.body.direction !== 'up' && req.body.direction !== 'down') {
+		return res.status(400).send('Not \'up\' or \'down\' parameter')
+	}
+
+	db.updateTopicVotesById(req.params.id, req.body.direction)
+		.then(topic => res.send(topic))
+		.catch(err => res.status(500).send(err))
+})
 
 
 // deletes a topic specified by id
